b1fe8e61b2c6ac7db5c179aeef750a487c9ba6fd
Oluwademilade Ala <demiladeala@gmail.com>
2023-01-23 21:41:07 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,4 +12,8 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  purge: {
+    content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"]
+    // These options are passed through directly to PurgeCSS
+  },
 }
