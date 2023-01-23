ea720eb72d5d73f3911b447b585df57e1699aee8
Oluwademilade Ala <demiladeala@gmail.com>
2023-01-23 21:48:20 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,13 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        montserrat:"Montserrat",
+      },
+    },
   },
   plugins: [],
-  purge: {
-    content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"]
-    // These options are passed through directly to PurgeCSS
-  },
 }
