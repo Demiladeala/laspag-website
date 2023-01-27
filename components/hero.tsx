68789478ed51389783c039f24c93a6527d78f000
Oluwademilade Ala <demiladeala@gmail.com>
2023-01-27 13:35:26 +0100

    laspag design updated

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,8 +1,8 @@
 import { motion } from 'framer-motion'
 import heroimg from '../public/mobile-hero-img.svg'
+import desktopheroimg from '../public/desktop-hero-img.svg'
 import Image from 'next/image'
 import React from 'react'
-import logo from '../public/logo.png'
 
 type Props = {}
 
@@ -13,25 +13,34 @@ export default function Hero({}: Props) {
     whileInView={{opacity:1}}
     transition={{duration:1}}
     viewport={{once: true}}
-    className='h-screen relative justify-evenly max-w-full bg-gray-300/5'>
-      <div className='flex flex-col gap-8 sm:flex-row h-screen bg-[#edcf7e]/5 w-full mx-auto p-4  items-center justify-center'>
+    className='h-screen hero-bg relative justify-evenly max-w-full'>
+      <div className='flex h-screen w-full items-center  text-white'>
 
-        <div className='mt-12 flex flex-col items-center justify-center text-center '>
-          <h1 className=' font-bold w-[95%] mt-0 pt-0  text-2xl sm:text-5xl  flex mx-auto text-gray-600 '>Delicacy like never before</h1>
-          <div className=' mx-auto mt-0 ml-4 w-full'>
-            <button className=' bg-[#D8A61F] max-[350px]:px-4 px-7 py-1 rounded-xl font-bold text-white hover:transition-all duration-1000'>Login</button>
-            <button className='border bg-[#D8A61F] ml-4 bg-opacity-10 max-[350px]:px-4 px-7 py-1 rounded-xl font-bold text-black hover:border hover:bg-opacity-10 hover:border-[#D8A61F] hover:text-gray-700 hover:transition-all duration-1000'>Sign up</button>
-          </div>
+      <motion.div 
+      initial={{x:-80+'%'}}
+      whileInView={{x:0}}
+      transition={{duration:1, type:[0.6,0.01,-0.05,0.95], delay:.2}}
+      viewport={{once:true}}
+      className='w-full flex flex-col justify-start px-4 pt-40 md:px-20 md:mt-36'
+      >
+        <h1 className='font-bold text-4xl font-montserrat md:text-8xl'>DELICACY</h1>
+        <p className=' font-seaweed text-3xl md:text-5xl ml-6 md:ml-28'>Like never before</p>
+        <p className='py-4 md:text-4xl'><span className='font-bold'>Spaghetti</span> and <span className='font-bold'>Chicken</span></p>
+        <motion.div 
+        initial={{y:100, opacity:0}}
+        animate={{opacity:1, y:0}}
+        transition={{ delay:1, type:'spring', stiffness:120}}
+        className='relative flex flex-row pt-8 md:mt-16 md:mx-40 cursor-pointer'>
+        <button className='text-black bg-gray-100 md:text-base px-4 py-1 mr-4 rounded-2xl shadow-xl shadow-[rgba(0, 0, 0, 0.25)] cursor-pointer'>Login</button>
+        <button className='text-black bg-gray-100 md:text-base px-4 py-1 rounded-2xl shadow-xl shadow-[rgba(0, 0, 0, 0.25)]  cursor-pointer'>Sign up</button>
+        </motion.div>
+      </motion.div>
+
+        <div className='border-white absolute right-0 '>
+          <Image alt='hero' src={heroimg} className='w-[500px]  visible md:hidden animate-pulse duration-1000'></Image>
+          <Image alt='desktop hero' src={desktopheroimg} className='w-[1000px] hidden md:flex md:animate-pulse md:duration-1000'></Image>
         </div>
 
-        <div className='flex justify-center'>
-          <Image alt='hero' src={heroimg} className='w-60 md:w-[400px] md:animate-pulse md:duration-1000'></Image>
-        </div>
-
-        <div className='hidden md:flex w-[50%] h-[87vh] right-0 bottom-0 absolute bg-[#edcf7e]/40 polygon overflow-x-hidden'>
-
-        </div>
-         
       </div>
     </motion.div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
   theme: {
     extend: {
       fontFamily:{
+        ptsans:"PT Sans Caption",
         montserrat:"Montserrat",
+        seaweed: "Seaweed Script",
       },
     },
   },
