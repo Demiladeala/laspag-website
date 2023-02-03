0e22869dfbfd1251a861a6e9c4e433a603f6b297
Praise <oyeniyipraise@gmail.com>
2023-02-03 16:08:42 +0100

    about

--- components/aboutUs.tsx
+++ components/aboutUs.tsx
@@ -16,14 +16,14 @@ export default function AboutUs({}: Props) {
         </div>
 
 
-        <div className="about-inner-top flex flex-start sm:items-center sm:space-x-10 sm:flex-row flex-col sm:space-y-0 space-y-5">
+        <div className="about-inner-top flex flex-start sm:items-center sm:space-x-20 sm:flex-row flex-col sm:space-y-0 space-y-5">
           <div className="about-image-top relative sm:place-self-center place-self-start sm:w-3/6 w-5/6">
             <div className='relative sm:h-72 sm:w-full bg-[#D8A61F] rounded-r-full w-full h-52 my-2 sm:my-0 '>
               <Image src={Babs} alt="Founder Image" className='absolute top-[50%] translate-y-[-50%] right-0 mr-2 w-auto h-[90%] sm:h-full sm:py-3 '></Image>
             </div>
 
           </div>
-          <div className="about-text-top sm:px-7 pl-3 border-l-4 border-l-[#D8A61F] space-y-1 w-5/6 mx-auto">
+          <div className="about-text-top sm:px-7 pl-3 border-l-[3px] border-l-[#D8A61F] space-y-1 w-5/6 mx-auto">
             <h2 className='font-bold sm:text-5xl text-xl'>ASULEWON BABALOLA</h2>
             <h4 className='sm:text-3xl font-normal text-sm'>FOUNDER & CEO</h4>
             <p className='sm:text-base font-normal pt-1 leading-tight sm:pr-5 text-base'>
@@ -41,7 +41,7 @@ export default function AboutUs({}: Props) {
 
 
         <div className="about-inner-bottom flex flex-start items-center sm:space-x-5 sm:w-[90%] w-5/6 mx-auto sm:flex-row flex-col  sm:space-y-0 space-y-5 " >
-          <div className="about-text-bottom sm:px-7 pl-3 border-l-4 border-l-[#D8A61F] sm:order-1 order-2">
+          <div className="about-text-bottom sm:px-7 pl-3 border-l-[3px] border-l-[#D8A61F] sm:order-1 order-2">
             <h2 className='font-bold sm:text-5xl text-xl'>LA-SPAG</h2>
             <p className='sm:text-base font-normal pt-2 leading-tight text-base'>
               lorem ipsum dolor sit amet lorem ipsum dolo sit amet lorem ipsum dolor sit amet lorem
