fea9d00102f5c488bae4fdb50977eb1bd29a6c1e
Oluwademilade Ala <demiladeala@gmail.com>
2023-02-03 20:52:37 +0100

    UPDATE

--- components/banner.tsx
+++ components/banner.tsx
@@ -13,7 +13,7 @@ export default function Banner({}: Props) {
         viewport={{once: true}}  
         className="banner w-5/6 mx-auto rounded-3xl bg-[#D8A61F] text-white sm:p-20 p-10  text-center sm:space-y-10 space-y-5 font-bold">
             <h2 className='sm:text-5xl text-sm'>Be a member and receive a discount of up to 10%</h2>
-            <button className='news-btn sm:h-16 h-8 rounded-3xl bg-white px-10 text-[#D8A61F] sm:text-lg text-[7px]'>Sign Up</button>
+            <button className='news-btn sm:h-16 h-8 rounded-3xl bg-white px-10 text-[#D8A61F] sm:text-lg text-[7px] hover:bg-white/80'>Sign Up</button>
         </motion.div>
     </div>
   )
